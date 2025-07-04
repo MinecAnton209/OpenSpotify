462ed847ad57b3b05b5c81eede929958d1d85383
MinecAnton209 <poterbaanton90@gmail.com>
2025-07-04 11:50:03 +0300

    Initial commit

---
+++ src/components/Player.tsx
@@ -0,0 +1,11 @@
+﻿const Player = () => {
+    return (
+        <footer className="bg-zinc-800 text-white p-4">
+            <div className="flex items-center justify-center h-full">
+                <p>Player will be here</p>
+            </div>
+        </footer>
+    );
+};
+
+export default Player;
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,16 @@
+﻿const Sidebar = () => {
+    return (
+        <aside className="w-64 bg-black p-4 shrink-0"> {/* Додано shrink-0 */}
+            <div className="text-white font-bold text-xl">OpenSpotify</div>
+            <nav className="mt-8">
+                <ul>
+                    <li className="text-gray-300 hover:text-white cursor-pointer">Home</li>
+                    <li className="text-gray-300 hover:text-white cursor-pointer mt-4">Search</li>
+                    <li className="text-gray-300 hover:text-white cursor-pointer mt-4">Your Library</li>
+                </ul>
+            </nav>
+        </aside>
+    );
+};
+
+export default Sidebar;
